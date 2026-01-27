e4570e8fcce8b5bda26d713181913aacd6f65676
saugat1070 <saugatgiri1070@gmail.com>
2026-01-27 10:42:33 +0545

    Service related for User

---
+++ backend/src/Service/user.service.ts
@@ -0,0 +1,45 @@
+
+import UserModel from "../Models/auth.model.js";
+import jwt from "jsonwebtoken";
+import Env from "../Config/envConfig.js";
+import bcrypt from "bcrypt";
+export const createUser = async(firstName:string,lastName:string,email:string,password:string)=>{
+    const user = await UserModel.create({
+        fullName : {
+            firstName,lastName
+        },
+        email : email,
+        password : password
+    }
+    );
+    return user;
+}
+
+export const UserfindById = async (_id:string)=>{
+    if(!_id){
+        throw new Error("Id must be provided");
+    }
+    const user = await UserModel.findById(_id).select("-password");
+    return user;
+}
+
+export const UserfindByEmail = async (email:string)=>{
+    if(!email){
+        throw new Error("Id must be provided");
+    }
+    const user = await UserModel.findOne({email:email});
+    return user;
+}
+
+export const generateAuthToken = function (id:any) {
+    const token = jwt.sign({ _id:id  }, Env.jwtSecret, { expiresIn: '24h' });
+    return token;
+}
+
+export const hashedPassword = function(password:string){
+    return  bcrypt.hashSync(password,10)
+}
+
+export const comparePassword =  function(password:string,hashedPassword:string){
+    return  bcrypt.compareSync(password,hashedPassword);
+}
