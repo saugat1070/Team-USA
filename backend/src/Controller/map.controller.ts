f9802df884ec8f8a23b91772b5399aa5e577539f
saugat1070 <saugatgiri1070@gmail.com>
2026-01-29 03:11:39 +0545

    Delete Seeding District

--- backend/src/Controller/map.controller.ts
+++ backend/src/Controller/map.controller.ts
@@ -1,36 +1,9 @@
 import { Request, Response } from "express";
 import DistrictModel from "../Models/district.model.js";
-import districtGeoJson from "../disrict_all_cord.json" with { type: "json" };
 import MapService from "../Service/map.service.js";
 import { IRequest } from "./auth.controller.js";
 import RoomService from "../Service/room.service.js";
 
-export const seedingDistricts = async (req: Request, res: Response) => {
-  try {
-    const filterData = (districtGeoJson as any).features.map(
-      (feature: any) => ({
-        name: feature.properties.adm2_name,
-        geometry: feature.geometry,
-      }),
-    );
-
-    // Insert data into database
-    const result = await DistrictModel.insertMany(filterData);
-
-    console.log("Districts seeded:", result.length);
-    return res.status(201).json({
-      success: true,
-      message: `${result.length} districts added`,
-    });
-  } catch (error) {
-    console.error("Seeding error:", error);
-    return res.status(500).json({
-      success: false,
-      message: "Error seeding districts",
-      error: error instanceof Error ? error.message : "Unknown error",
-    });
-  }
-};
 
 export const getUserCooridnate = async (req: IRequest, res: Response) => {
   try {
