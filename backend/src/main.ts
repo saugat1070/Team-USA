4650547f16e3fc68bc1bd7ef681ea6a6c20ff663
saugat1070 <saugatgiri1070@gmail.com>
2026-01-29 02:03:54 +0545

    Other related files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import logger from "./utils/logger.js";
 import userRoutes from "./Routes/auth.routes.js";
 import mapRoutes from "./Routes/map.routes.js";
 import { leaderRoute } from "./Routes/userWalk.routes.js";
+import blogRoutes from "./Routes/blog.routes.js";
 
 export const app = express();
 app.use(express.json());
@@ -13,10 +14,12 @@ app.use(cors({
     origin : "*",
     credentials : true
 }));
+app.use("/uploads", express.static("uploads"));
 
 app.use("/api/v1",userRoutes);
 app.use("/api/v1",mapRoutes);
 app.use("/api/v1",leaderRoute);
+app.use("/api/v1",blogRoutes);
 
 
 connectDb(); // Database Connection
