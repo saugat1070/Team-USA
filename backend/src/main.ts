ac12f384cb518008324adbb14967f3b931d74ad0
saugat1070 <saugatgiri1070@gmail.com>
2026-01-29 08:28:36 +0545

    Adding root route for Welcome all

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import userRoutes from "./Routes/auth.routes.js";
 import mapRoutes from "./Routes/map.routes.js";
 import { leaderRoute } from "./Routes/userWalk.routes.js";
 import blogRoutes from "./Routes/blog.routes.js";
+import { timeStamp } from "node:console";
 
 export const app = express();
 app.use(express.json());
@@ -14,12 +15,24 @@ app.use(cors({
     origin : "*",
     credentials : true
 }));
+
+app.get("/", (_req, res) => {
+    return res.status(200).json({
+        message: "Welcome to Paila API",
+        status: "ok",
+        timeStamp: new Date().toISOString()
+    });
+});
+
+
+
 app.use("/uploads", express.static("uploads"));
 
 app.use("/api/v1",userRoutes);
 app.use("/api/v1",mapRoutes);
 app.use("/api/v1",leaderRoute);
 app.use("/api/v1",blogRoutes);
+
 
 
 connectDb(); // Database Connection
